075bc84686d2b80e5fad59656b5a3d4ec7abcd6d
uzairjanmalik <muhammaduzair1528@gmail.com>
2024-02-22 01:57:35 -0700

    even and odd using js

---
+++ index.js
@@ -0,0 +1,3 @@
+const user= prompt(`enter a number to see it,s even or odd.`);
+
+user%2 == 0 ? document.write(`<center><h1>THE NUMBER YOU HAVE INPUT IS EVEN</center></h1>`): document.write(`<center><h1>THE NUMBER YOU HAVE INPUT IS ODD.</center></h1>`)
